89e44b041b4a2edabc92b33bfc337bb083d670df
whit3rabbit <whiterabbit@protonmail.ch>
2024-06-25 16:27:24 -0500

    reorganize names

--- nodes/crewai.py
+++ nodes/crewai.py
@@ -254,7 +254,7 @@ class OpenAILLMNode:
     RETURN_NAMES = ()
     FUNCTION = "set_openai_llm"
     OUTPUT_NODE = True
-    CATEGORY = "Crewai/LLMs"
+    CATEGORY = "Crewai/LLMs/OpenAI"
  
     def set_openai_llm(self, api_key, model, base_url="https://api.openai.com/v1", max_tokens=1000, temperature=0.7):
         if not api_key:
@@ -279,7 +279,7 @@ class ClaudeLLMNode:
     RETURN_NAMES = ()
     FUNCTION = "set_claude_llm"
     OUTPUT_NODE = True
-    CATEGORY = "Crewai/LLMs"
+    CATEGORY = "Crewai/LLMs/Claude"
  
     def set_claude_llm(self, api_key, model, max_tokens=1000, temperature=0.7):
         if not api_key:
@@ -304,46 +304,12 @@ class GroqLLMNode:
     RETURN_NAMES = ()
     FUNCTION = "set_groq_llm"
     OUTPUT_NODE = True
-    CATEGORY = "Crewai/LLMs"
+    CATEGORY = "Crewai/LLMs/Groq"
  
     def set_groq_llm(self, api_key, model, max_tokens=1000, temperature=0.7):
         if not api_key:
             raise ValueError("API key is required")
         return (ChatOpenAI(base_url="https://api.groq.com/openai/v1", api_key=api_key, model=model, max_tokens=max_tokens, temperature=temperature),)
-
-class LLMNode:
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "provider": (["OpenAI", "Claude"],),
-                "api_key": ("STRING", {"default": ""}),
-             },
-            "optional": {
-                "base_url": ("STRING", {"default": "https://api.openai.com/v1"}),
-                "openai_model": (["gpt-4", "gpt-3.5-turbo"], {"default": "gpt-3.5-turbo"}),
-                "claude_model": (["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"], {"default": "claude-3-opus-20240229"}),
-                "max_tokens": ("INT", {"default": 1000, "min": 1, "max": 100000}),
-                "temperature": ("FLOAT", {"default": 0.7, "min": 0.0, "max": 1.0, "step": 0.1}),
-            }
-        }
- 
-    RETURN_TYPES = ("LLM",)
-    RETURN_NAMES = ()
-    FUNCTION = "set_llm"
-    OUTPUT_NODE = True
-    CATEGORY = "Crewai"
- 
-    def set_llm(self, provider, api_key, base_url=None, openai_model=None, claude_model=None, max_tokens=1000, temperature=0.7):
-        if not api_key:
-            raise ValueError("API key is required")
-
-        if provider == "OpenAI":
-            return (ChatOpenAI(base_url=base_url, api_key=api_key, model=openai_model, max_tokens=max_tokens, temperature=temperature),)
-        elif provider == "Claude":
-            return (ClaudeLLM(api_key=api_key, model=claude_model, max_tokens=max_tokens, temperature=temperature),)
-        else:
-            raise ValueError(f"Unsupported provider: {provider}")
 
 class AgentListNode:
     def __init__(self):
@@ -603,9 +569,9 @@ NODE_CLASS_MAPPINGS = {
     "Crew": CrewNode,
     "Agent": AgentNode,
     "Task": TaskNode,
-    "OpenAILLM": OpenAILLMNode,
-    "ClaudeLLM": ClaudeLLMNode,
-    "GroqLLM": GroqLLMNode,
+    "OpenAI LLM": OpenAILLMNode,
+    "Claude LLM": ClaudeLLMNode,
+    "Groq LLM": GroqLLMNode,
     "AgentList": AgentListNode,
     "TaskList": TaskListNode,
     "ToolsList": ToolsListNode,
@@ -614,24 +580,22 @@ NODE_CLASS_MAPPINGS = {
     "FRT": FRTNode,
     "MDXST": MDXSTNode,
     "ContextList": ContextListNode,
-
 }
  
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CrewtNode": "Node of Crew",
-    "AgentNode": "Node of Agent",
-    "TaskNode": "Node of Task",
+    "CrewNode": "Crew",
+    "AgentNode": "Agent",
+    "TaskNode": "Task",
     "OpenAILLMNode": "OpenAI LLM",
     "ClaudeLLMNode": "Claude LLM",
     "GroqLLMNode": "Groq LLM",
-    "AgentListNode": "Node of AgentList",
-    "TaskListNode": "Node of TaskList",
-    "ToolsListNode": "Node of ToolsList",
-    "SWTNode": "Node of SWT", 
-    "SDTNode": "Node of SDT",
-    "FRTNode": "Node of FRT",
-    "MDSXSTNode": "Node of MDXST",
-    "ContextListNode": "Node of ContextList",
+    "AgentListNode": "AgentList",
+    "TaskListNode": "TaskList",
+    "ToolsListNode": "ToolsList",
+    "SWTNode": "Scrape Website Tool", 
+    "SDTNode": "Serper Dev Tool",
+    "FRTNode": "File Read Tool",
+    "MDXSTNode": "MDX Search Tool",
+    "ContextListNode": "ContextList",
 }
- 
